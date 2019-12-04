90fea88f3c1965f5b1b976cad8cdea66d7c7287d
Angel Kozlev <angelkozlev@gmail.com>
2019-12-04 19:00:47 +0000

    wires bruh

---
+++ day03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace day03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text = File.ReadAllText(@Directory.GetCurrentDirectory() + "/input/part").Split('\n');
+
+            List<char, int> wire1;
+            List<char, int> wire2;
+
+            Console.WriteLine();
+        }
+    }
+}
